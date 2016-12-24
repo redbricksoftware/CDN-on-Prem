b57c16e3a028f6a6e32c59d9d439799ff72011eb
rbsftwr <pietasterkid@gmail.com>
2016-12-24 17:17:20 -0500

    committed

--- server.js
+++ server.js
@@ -1,149 +1,192 @@
-var express = require("express");
+'use strict;'
+
+var libraryFolder = '/Users/BryceD/Documents/cdnlibrary/';
+var minutesCacheTime = 15;
+var mongoURI = '10.211.55.7';
+var mongoPORT = '27017';
+var mongoDB = 'testdb';
+
+
+//Libraries
+var express = require('express');
+var logger = require('morgan');
 var mongoose = require('mongoose');
 var Grid = require('gridfs-stream');
 Grid.mongo = mongoose.mongo;
-var fs = require("fs");
+//var memCache = require('memory-cache');
+var timer = require('timers');
+var fs = require('fs');
+const util = require('util');
+const path = require('path');
+
+var option = {encoding: 'utf-8'};
 
 var app = express();
 
-var option = {encoding: "utf-8"};
-var libraryFolder = '/Users/BryceD/Documents/cdnlibrary/';
-
+//return file if found
 app.use(express.static(libraryFolder));
 
-app.get("/", function (request, response) {
-    var currentDate = new Date();
-    response.send("CDN Home" + currentDate);
+syncLocalFileLibrary();
+timer.setInterval(syncLocalFileLibrary, minutesToMS(minutesCacheTime));
+
+app.get('/refresh', function (request, response) {
+    syncLocalFileLibrary();
+    response.send('refreshed');
 });
 
-app.get("/refresh", function (request, response) {
-    refreshLibrary();
-    response.send("refreshed");
-});
+app.get('*', function(request, response){
+    response.statusCode = 404;
+    response.send('file not found');
+})
 
 app.listen(3000, function () {
-    console.log("express app started on 3000.");
+    console.log('express app started on 3000.');
 });
 
 
-refreshLibrary2 = function () {
-    console.log("refresh");
-    fs.readdir(libraryFolder, function (err, files) {
-        if (err) {
-            console.error("Could not list the directory.", err);
-            //process.exit( 1 );
-        }
-        else {
 
-            files.forEach(function (file, index) {
-                // Make one pass and make the file complete
-                //var fromPath = path.join(moveFrom, file);
-                //var toPath = path.join(moveTo, file);
-                console.log(file);
-            });
-        }
+function syncLocalFileLibrary() {
+
+    var conn = mongoose.createConnection('mongodb://' + mongoURI + ':' + mongoPORT + '/' + mongoDB);
+    conn.on('error', console.error.bind(console, 'connection error:'));
+    conn.once('open', function () {
+
+        var gfs = Grid(conn.db);
+
+        //gfs.files.find({'metadata.deleted': false}).toArray(function (err, files) {
+
+        //return all files
+        gfs.files.find({}).toArray(function (err, files) {
+            if (err) {
+                console.log('find files error');
+                console.log(err);
+            } else {
+                console.log('files found:');
+
+                var currentDate = new Date();
+
+                //iterate through files.
+                for (x = 0; x < files.length; x++) {
+
+                    var file = path.join(libraryFolder, files[x].filename);
+
+                    if (files[x].metadata.deleted) {
+                        //If file is flagged as to delete and exists, delete file.
+                        fs.unlink(file, (err) => {
+                            if (err) {
+                                console.log('file ' + file + '  not found or not readable');
+                            } else {
+                                console.log('file deleted');
+                            }
+                        });
+
+                    } else if (files[x].metadata.modifiedDate) {
+                        //check if filed modified and needs to be replaced.
+                        var modifiedDate = new Date(Date.parse(files[x].metadata.modifiedDate));
+
+                        if (Object.prototype.toString.call(modifiedDate) === "[object Date]") {
+                            // it is a date
+
+                            if (isNaN(modifiedDate.getTime())) {  // d.valueOf() could also work
+                                // date is not valid
+                            } else {
+                                // date is valid
+
+                                var fileStat = fs.stat(file, (err, stats) => {
+                                    if (err) {
+                                        console.log('file ' + file + '  not found or not readable');
+                                    } else {
+                                        if (stats.ctime < modifiedDate) {
+                                            console.log('need to update file: ' + file + '. modified date: ' + modifiedDate + ' - fileChangeTime: ' + stats.ctime);
+                                            fs.unlink(file, (err) => {
+                                                if (err) {
+                                                    console.log('file not found or not deleteable');
+                                                } else {
+                                                    console.log('file deleted');
+                                                }
+                                            });
+                                        }
+                                    }
+                                });
+
+                                //TODO: write file
+                            }
+                        } else {
+                            // not a date
+                        }
+
+                    } else {
+                        //check if file exists
+
+                        fs.stat(file, function (err, stats) {
+                            //file not found
+                            if (err) {
+                                //return console.error('file not found: ' + this.fileName + ' ID: ' + this.fileID + '. Downloading file to library');
+                                console.error('file not found: ' + this.fileName + ' ID: ' + this.fileID + '. Downloading file to library');
+
+                                // streaming to gridfs
+                                var fileReadStream = gfs.createReadStream({
+                                    _id: this.fileID
+                                });
+
+                                var newFileName = path.join(libraryFolder, this.fileName);
+
+                                console.log('writeFile: ' + newFileName);
+                                var fileWriteStream = fs.createWriteStream(newFileName);
+
+                                fileReadStream.pipe(fileWriteStream);
+
+                                fileReadStream.on('error', function (err) {
+                                    console.log('error: ' + err);
+                                });
+
+                            }
+                        }.bind({fileName: files[x].filename, fileID: files[x]._id}));
+                    }
+                    console.log('fileName: ' + files[x].filename);
+
+                }
+            }
+        });
+
+    }, function (err) {
+        console.log(err);
+        console.log('fin error');
     });
-};
 
-var mongoURI = '10.211.55.3';
-var mongoPORT = '';
-refreshLibrary = function () {
-    //mongoose.connect('mongodb://localhost/test');
+}
+
+function minutesToMS(minutes){
+    return minutes * 60 * 1000;
+}
 
 
+function addFileToDB() {
     var conn = mongoose.createConnection('mongodb://' + mongoURI + ':27017/testdb');
     conn.on('error', console.error.bind(console, 'connection error:'));
     conn.once('open', function () {
 
+        var gfs = Grid(conn.db);
 
-            var gfs = Grid(conn.db);
+        // streaming to gridfs
+        var writestream = gfs.createWriteStream();
 
-            //gfs.files.find({'metadata.deleted': false}).toArray(function (err, files) {
-            //return all files
-            gfs.files.find({}).toArray(function (err, files) {
-                if (err) {
-                    console.log(err);
-                }
-                var currentDate = new Date();
+        var file = path.join(libraryFolder + 'sample.txt');
 
-                for (x = 0; x < files.length; x++) {
-                    console.log(files[x]);
+        var readStream = fs.createReadStream(file);
+        readStream.on('error', function (err) {
+            console.log('error: ' + err);
+        });
 
-                    //Remove files from library that are deleted
-                    if (files[x].metadata) {
-                        if (files[x].metadata.deletedEffectiveDate) {
-                            var deletedEffectiveDate = new Date(files[x].metadata.deletedEffectiveDate);
-                        } else {
-                            var deletedEffectiveDate = new Date(2000, 1, 1);
-                        }
-                        ;
 
-                        if (files[x].metadata.deleted && currentDate >= deletedEffectiveDate) {
-                            //get deletion effective date
-                            fs.unlink(libraryFolder + files[x].filename, function (err) {
-                                if (err) {
-                                    console.log('file does not exist: ' + this.fileName);
-                                    //console.log(err);
-                                } else {
-                                    console.log('file deleted successfully: ' + this.fileName);
-                                }
-                            }.bind({fileName: files[x].filename}));
-                        }
-                    }
-
-                    fs.stat(libraryFolder + files[x].fileName, function (err, stats) {
-                        //console.log(stats);
-
-                        if (err) {
-                            return console.error('file not found: ' + err);
-                        }
-                    }.bind({fileName: files[x].filename}));
-
-                }
+        //write the file stream to GridFS
+        readStream.on('readable', function () {
+            var writestream = gfs.createWriteStream({
+                filename: 'sampleFileName', //TODO: make variable
+                metadata: {'modifiedDate': null, 'deleted': false}
             });
+            readStream.pipe(writestream);
+        });
+    });
 
-            /*console.log('totally connected!');
-
-             //TODO: don't write a file, setup a URL to write the file and check if file already in GridFS before overwriting
-             //display all files in dir
-             fs.readdir(libraryFolder, function (err, files) {
-             if (err) {
-             console.error("Could not list the directory.", err);
-             }
-             else {
-
-             files.forEach(function (file, index) {
-             console.log(file);
-             });
-             }
-             });
-
-             //Library location
-             console.log(libraryFolder);
-
-             // streaming to gridfs
-             var writestream = gfs.createWriteStream();
-             var readStream = fs.createReadStream(libraryFolder + 'sample.txt');
-             readStream.on('error', function(err){
-             console.log('error: ' + err);
-             });
-
-             //write the file stream to GridFS
-             readStream.on('readable', function () {
-             var writestream = gfs.createWriteStream({
-             filename: 'sampleFileName', //TODO: make variable
-             metadata: {'modifiedDate':null, 'deleted': false}
-             });
-             readStream.pipe(writestream);
-             });*/
-
-
-            conn.close();
-        },
-        function (err) {
-            console.log(err);
-        }
-    );
-
-};
-
+}
